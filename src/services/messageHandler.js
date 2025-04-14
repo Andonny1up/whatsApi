b192be42caf3d7aa9dc335bf4f24656a7c5f0190
Andoni1Barba <Andoni1barba@gmail.com>
2025-04-14 09:46:31 -0400

    Saludo inicial

--- src/services/messageHandler.js
+++ src/services/messageHandler.js
@@ -1,12 +1,30 @@
-import whatsappService from "./whatsappService";
+import whatsappService from "./whatsappService.js";
 
 class MessageHandler{
     async handleIncomingMessage(message){
         if(message?.type === 'text'){
-            const response = `Echo: ${message.text.body}`;
-            await whatsappService.sendMessage(message.from,response,message.id);
-            await whatsappService.markAsRead(message.id);
+            const incomingMessage = message.text.body.toLowerCase().trim();
+            if (this.isGreating(incomingMessage)) {
+                await this.sendWelcomeMessage(message.from, message.id)
+            }else{
+                const response = `Echo: ${message.text.body}`;
+                await whatsappService.sendMessage(message.from,response,message.id);
+                await whatsappService.markAsRead(message.id);
+            }
+            
         }
+    }
+
+    isGreating(message){
+        const greetings = ["hola","buenas tarde","buenos dias","buenas noches"]
+
+        return greetings.includes(message)
+    }
+
+    async sendWelcomeMessage(to, messageId){
+        const welcomeMessage = "Hola, Bienvenido a Narnia." + "¿En qué puedo ayudarte hoy?";
+        await whatsappService.sendMessage(to, welcomeMessage, messageId);
+        await whatsappService.markAsRead(messageId);
     }
 }
 
